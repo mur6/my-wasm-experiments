61af3b7eb27f5d9619b3186c78eb200fcf93082c
MURAKI Taichi <muraki.taichi@gmail.com>
2021-06-18 16:54:22 +0900

    Fix leb128 test program.

--- leb128/main.py
+++ leb128/main.py
@@ -1,15 +1,13 @@
-import io
 import leb128
 
-# unsigned leb128
-assert leb128.u.encode(624485) == bytearray([0xe5, 0x8e, 0x26])
-assert leb128.u.decode(bytearray([0xe5, 0x8e, 0x26])) == 624485
-assert leb128.u.decode_reader(io.BytesIO(bytearray([0xe5, 0x8e, 0x26]))) == (624485, 3)
+def conv_leb128(num):
+    leb128_byte_array = leb128.u.encode(num)
+    return ",".join(f"{b:02x}" for b in leb128_byte_array)
 
-# signed leb128
-assert leb128.i.encode(-123456) == bytearray([0xc0, 0xbb, 0x78])
-assert leb128.i.decode(bytearray([0xc0, 0xbb, 0x78])) == -123456
-assert leb128.i.decode_reader(io.BytesIO(bytearray([0xc0, 0xbb, 0x78]))) == (-123456, 3)
+def main():
+    for i in range(300):
+        h = '{:02x}'.format(i)
+        q = conv_leb128(i)
+        print(f"{i:03}\t0x{i:04x}\tLEB128=[{q}]")
 
-print("unsigned leb128")
-print(leb128.u.encode(12))
+main()
